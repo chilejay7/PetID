bd1a57bf8bb81078a79272ae2a7ac7ace41a214a
chilejay7 <codyburk7@yahoo.com>
2024-07-06 17:59:18 -0600

    updated menu option 2

--- Program.cs
+++ Program.cs
@@ -138,6 +138,13 @@ do
             if (ourAnimals[i, 0] != "ID #: ") {
                 petCount += 1;
             }
+
+            if (petCount < maxPets) {
+                Console.WriteLine($"We currently have {petCount} pets that need homes.  We can manage {(maxPets - petCount)} more.");
+            }
+
+            Console.WriteLine(petCount);
+
            }
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
