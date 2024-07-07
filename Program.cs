8defcdad05bde9707fdec24105cf8c6dea472e46
chilejay7 <codyburk7@yahoo.com>
2024-07-06 18:09:59 -0600

    creates function to add a pet

--- Program.cs
+++ Program.cs
@@ -150,9 +150,31 @@ do
                 Console.WriteLine($"We currently have {petCount} pets that need homes.  We can manage {(maxPets - petCount)} more.");
             }
 
-            Console.WriteLine("Press the Enter key to continue.");
+            while (anotherPet == "y" && petCount < maxPets)
+            {
+                if (petCount < maxPets)
+                {
+                    Console.WriteLine("Do you want to enter infor for another pet (y/n)?");
 
-            readResult = Console.ReadLine();
+                    do
+                    {
+                        readResult = Console.ReadLine();
+                        if (readResult != null)
+                        {
+                            anotherPet = readResult.ToLower();
+                        }
+                    }
+                    while (anotherPet != "y" && anotherPet != "n");
+                }
+            }
+
+            if (petCount >= maxPets)
+            {
+                Console.WriteLine("We have reached our limit on the number of pets that we can manage.");
+                Console.WriteLine("Press the Enter key to continue.");
+                readResult = Console.ReadLine();
+            }
+
             break;
 
         case "3":
